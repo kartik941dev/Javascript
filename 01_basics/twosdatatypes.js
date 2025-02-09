9679efc27d1b415ffcfd2610623bb1ffccae8e41
kartik941dev <144128354+kartik941dev@users.noreply.github.com>
2025-02-09 13:45:08 +0000

    datadtypes

---
+++ 01_basics/twosdatatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version
+//alert(3 + 3) we are usng nodejs, not browser
+
+console.log(3+3); // code readlibility shoudl be high
+console.log("Hitesh")
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false 
+let state = null
+
+// number => 2 to the power 53
+// bigint
+// String
+// boolean => true/false
+// null => standalone value(representaion of null value in specialtype)
+// Undefined => value is not assigned.
+//symbol => uinqueness
+// object => detailed video discussion
+console.log(typeof undefined)
+//type of undefined is undefined but type of null is object !!!
