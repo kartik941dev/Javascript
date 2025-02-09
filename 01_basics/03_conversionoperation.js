9faa952b4efc9b3e392f73d28949462fa2bcbb1e
kartik941dev <144128354+kartik941dev@users.noreply.github.com>
2025-02-09 14:25:16 +0000

    conversion03

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,25 @@
+let score = undefined
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+//NaN is a special type 
+// "33" =>
+// true => 1 ; falswe => 0
+
+
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1=> true; 0=>false
+// "hitesh" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
