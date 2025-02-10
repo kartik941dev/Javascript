84ca70cb3ddc22bec20050ba45363069c74baab4
kartik941dev <144128354+kartik941dev@users.noreply.github.com>
2025-02-10 01:07:11 +0000

    comparison

---
+++ 01_basics/comparison.js
@@ -0,0 +1,10 @@
+console.log(null > 0);
+console.log(null == 0);
+console.log(null>=0);
+
+console.log(undefined == 0);
+// in most of the cases we will avoid these types of conversions.
+// undefined always returns false in all cases.
+
+// == is only for conversion
+// === will check values strictly and also check its datatypes.
