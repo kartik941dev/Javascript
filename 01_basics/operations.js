e94aeccae360cdbdaac361799882afbce5f45a70
kartik941dev <144128354+kartik941dev@users.noreply.github.com>
2025-02-10 01:10:29 +0000

    operation

---
+++ 01_basics/operations.js
@@ -0,0 +1,27 @@
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+
+//output => -3
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+//output =>
+// 12
+// 12
+// 122
+// 32
+// Dont use these codes because these looks so messy.
+
+console.log((+true));
+console.log(+"");
+
+//Output => 
+// 0
+// 1
+
+// go to prefix and suffix js mdn
