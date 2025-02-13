3b56262f163ded236d16b6da71fa861a81f05c52
kartik941dev <144128354+kartik941dev@users.noreply.github.com>
2025-02-13 00:06:31 +0000

    numandmaths.js

---
+++ 01_basics/numandamaths.js
@@ -0,0 +1,49 @@
+// const score = 400
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2)); //for e-commerce app it uses mainly
+
+// const otherNumber = 23.5658
+// console.log(otherNumber.toPrecision(3)); // output = 23.6
+
+// const tryNumber = 123.8966
+// console.log(tryNumber.toPrecision(3)); // output = 124
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString());// it converts in US type 
+//Output = 1,000,000
+
+
+// const anotherhundreds = 10000000
+// console.log(anotherhundreds.toLocaleString('en-IN'));
+//Output = 1,00,00,000
+
+
+//++++++++++++++++++++++++++ MATHS ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.56120));
+// console.log(Math.ceil(4.2));//it round to the upper value to control round function
+// console.log(Math.floor(4.9));// it round to the lower value to control round function
+console.log(Math.random());//value between 0 and 1;
+console.log(Math.floor(Math.random()*10 )+ 1); //we use +1 to avoid the value of 0
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max - min + 1)) + min) //***************
+
+// above line is important because in case of min max we use the above formula
+
+
+
+
+
+
+
+
+
